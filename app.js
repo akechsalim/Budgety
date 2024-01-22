a72803c3a9b23fa36343251c4457daaa40a1ebe2
akechsalim <akechsalimwork@gmail.com>
2024-01-22 12:14:09 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,12 @@
+var budgetController = (function () {
+    //some code
+})();
+
+var UIController = (function () {
+    //some code
+})();
+
+var controller = (function (budgetCtrl, UICtrl) {
+    //some code
+})(budgetController, UIController);
+
