b65b8851aea4e1af17f6aea4205a349baff8dd0d
akechsalim <akechsalimwork@gmail.com>
2024-01-23 18:15:40 +0300

    changes

--- app.js
+++ app.js
@@ -1,26 +1,96 @@
 //BUDGET CONTROLLER
 var budgetController = (function () {
-    //some code
+    var Expense = function (id, description, value) {
+        this.id = id;
+        this.description = description;
+        this.value = value;
+    };
+    var Income = function (id, description, value) {
+        this.id = id;
+        this.description = description;
+        this.value = value;
+    };
+
+    var data = {
+        allItems: {
+            exp: [],
+            inc: []
+        },
+        totals: {
+            exp: 0,
+            inc: 0
+        }
+    }
+
+    return {
+        addItem: function (type, des, val) {
+            var newItem;
+            var id = 0;
+
+            if (type === 'exp') {
+                newItem = new Expense(id, des, val);
+            } else if (type === 'inc') {
+                newItem = new Income(id, des, val);
+            }
+            data.allItems[type].push(newItem);
+            return newItem;
+        }
+    };
 })();
 
 // UI CONTROLLER
 var UIController = (function () {
-    //some code
+
+    var DOMStrings = {
+        inputType: '.add__type',
+        inputDescription: '.add__description',
+        inputValue: '.add__value',
+        inputBtn: '.add__btn'
+
+    };
+
+    return {
+        getInput: function () {
+            return {
+                type: document.querySelector(DOMStrings.inputType).value,
+                description: document.querySelector(DOMStrings.inputDescription).value,
+                value: document.querySelector(DOMStrings.inputValue).value
+            };
+
+        },
+        getDOMStrings: function () {
+            return DOMStrings;
+        }
+
+    };
 })();
 
 // GLOBAL APP CONTROLLER
 var controller = (function (budgetCtrl, UICtrl) {
 
+    var setUpEventListeners = function () {
+        var DOM = UICtrl.getDOMStrings();
+
+        document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddItem);
+
+        document.addEventListener('keypress', function (event) {
+            if (event.which === 13) {
+                ctrlAddItem();
+            }
+        });
+
+    };
     var ctrlAddItem = function () {
-        console.log('It works')
+        var input = UICtrl.getInput();
+        console.log(input);
     }
-
-    document.querySelector('.add__btn').addEventListener('click', ctrlAddItem);
-
-    document.addEventListener('keypress', function (event) {
-        if (event.which === 13) {
-            ctrlAddItem();
+    return {
+        init: function () {
+            setUpEventListeners();
         }
-    });
+    };
+
 })(budgetController, UIController);
+
+controller.init();
 
