a099c3421a0704489ca27d607fbd9671eda42d8b
akechsalim <akechsalimwork@gmail.com>
2024-01-23 12:11:13 +0300

    changes

--- app.js
+++ app.js
@@ -1,12 +1,26 @@
+//BUDGET CONTROLLER
 var budgetController = (function () {
     //some code
 })();
 
+// UI CONTROLLER
 var UIController = (function () {
     //some code
 })();
 
+// GLOBAL APP CONTROLLER
 var controller = (function (budgetCtrl, UICtrl) {
-    //some code
+
+    var ctrlAddItem = function () {
+        console.log('It works')
+    }
+
+    document.querySelector('.add__btn').addEventListener('click', ctrlAddItem);
+
+    document.addEventListener('keypress', function (event) {
+        if (event.which === 13) {
+            ctrlAddItem();
+        }
+    });
 })(budgetController, UIController);
 
