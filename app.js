74da329cac0fd10180d2615110994305139b54b9
akechsalim <akechsalimwork@gmail.com>
2024-02-06 19:33:58 +0300

    changes

--- app.js
+++ app.js
@@ -58,6 +58,20 @@ var budgetController = (function () {
             return newItem;
         },
 
+        deleteItem: function (type, id) {
+            var ids, index;
+
+            ids = data.allItems[type].map(function (current) {
+                return current.id;
+            })
+
+            index = ids.indexOf(id);
+
+            if (index !== -1) {
+                data.allItems[type].splice(index, 1);
+            }
+        },
+
         calculateBudget: function () {
 
             // calculate total income and expenses
@@ -244,7 +258,7 @@ var controller = (function (budgetCtrl, UICtrl) {
                 ID = splitID[1];
 
                 // 1. delete the item from the data structure
-                
+
                 //2. Delete the item from the UI
 
                 //3. Update and show the new budget
